31976f32bb0884188e2f646da2ba46b8dd239ebc
evanaaron26 <evanaaron26@yahoo.com>
2017-01-17 20:37:25 -0600

    fourth step

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 
-x = ModelName.new({color: "red", year: "2017", manufacture: "Ford", price "20000", image "https://s-media-cache-ak0.pinimg.com/originals/61/55/b9/6155b9d8557c4070a8e7c63622a0c07a.jpg", name: "Ford Raptor"})
+x = Car.new({
+	color: "red", 
+	year: "2017", 
+	manufacture: "Ford", 
+	price: "20000", 
+	image: "https://s-media-cache-ak0.pinimg.com/originals/61/55/b9/6155b9d8557c4070a8e7c63622a0c07a.jpg", 
+	name: "Ford Raptor"})
 x.save
 
