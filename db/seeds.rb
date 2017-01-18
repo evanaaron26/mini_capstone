c432620bbc84fea1c0dd313405b26b76e7dc6f6a
evanaaron26 <evanaaron26@yahoo.com>
2017-01-17 21:00:52 -0600

    added other models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 
 x = Car.new({
-	color: "red", 
-	year: "2017", 
-	manufacture: "Ford", 
-	price: "20000", 
-	image: "https://s-media-cache-ak0.pinimg.com/originals/61/55/b9/6155b9d8557c4070a8e7c63622a0c07a.jpg", 
-	name: "Ford Raptor"})
+	color: "black", 
+	year: "2014", 
+	manufacture: "Mitsubishi", 
+	price: "10000", 
+	image: "http://carsmag.us/wp-content/uploads/2014/07/2014-Mitsubishi-Lancer-GT-Black.jpg", 
+	name: "Mitsubishi Lancer"
+x.save
+
+x = Car.new({
+	color: "blue", 
+	year: "2013", 
+	manufacture: "kia", 
+	price: "30000", 
+	image: "http://www.optimaforums.com/forum/attachments/optima-general-discussion/11817d1350503387-2013-corsa-blue-sx-limited-here-new-color-sxl-bluesxl2.jpg", 
+	name: "2013 Corsa"
 x.save
 
