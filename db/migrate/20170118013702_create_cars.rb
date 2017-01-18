5e046df4112d2a0215a14404ad5b589ef4a91633
evanaaron26 <evanaaron26@yahoo.com>
2017-01-17 19:42:06 -0600

    update

--- db/migrate/20170118013702_create_cars.rb
+++ db/migrate/20170118013702_create_cars.rb
@@ -4,6 +4,9 @@ class CreateCars < ActiveRecord::Migration[5.0]
       t.string :color
       t.string :year
       t.string :manufacture
+      t.string :price
+      t.string :image
+      t.string :name
 
       t.timestamps
     end
