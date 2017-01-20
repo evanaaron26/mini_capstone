5692b9097f5ba02f57a1f512a3d082b78d0e2daa
evanaaron26 <evanaaron26@yahoo.com>
2017-01-20 10:41:10 -0600

    update to mini_capstone

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,28 @@ class CarsController < ApplicationController
 	def cars
 		@cars = Car.all 
 	end 
+
+    def getit 
+        
+    end
+
+    def useit 
+        @car = Car.new(
+
+            color: params["color"],
+            year: params["year"],
+            year: params["manufacture"],
+            price: params["price"],
+            image: params["image"],
+            color: params["color"],
+            name: params["name"]
+            
+            )
+
+        @car.save
+
+        render cars.html.erb
+    end
 end
+
+
